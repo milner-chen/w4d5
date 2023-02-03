0504f8d4187765dfd96b5acb32e7652ed8e3d9dd
Milner Chen <milnerchen0@gmail.com>
2023-02-03 14:10:49 -0500

    max

--- big_o.rb
+++ big_o.rb
@@ -9,7 +9,7 @@ def my_min(list)
 end
 
 
-p my_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
+# p my_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
 
 def my_better_min(list)
     min = list[0]
@@ -18,5 +18,23 @@ def my_better_min(list)
     min
 end
 
-p my_better_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
+# p my_better_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
 
+def largest_contiguous_subsum(list)
+    max = list.first
+    list.each_with_index do |n1, i1|
+        max = n1 if n1 > max
+        list.each_with_index do |n2, i2|
+            if i2 > i1
+                sum = list[i1..i2].sum
+                max = sum if sum > max
+            end
+        end
+    end
+    max
+end
+
+# list = [5, 3, -7]
+# list = [2, 3, -6, 7, -6, 7]
+list = [-5, -1, -3]
+p largest_contiguous_subsum(list)
